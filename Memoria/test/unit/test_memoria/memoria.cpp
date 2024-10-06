a94dd0ccfeb1520aae5aeac0f4dc3beb73799fb7
José Hipólito Moyano <josehmoyano@gmail.com>
2024-10-06 19:00:52 -0300

    [RTD-38] Haciendo funcionar tests unitarios sobre Preferences

--- Memoria/test/unit/test_memoria/memoria.cpp
+++ Memoria/test/unit/test_memoria/memoria.cpp
@@ -1,74 +1,53 @@
-
 #include <unity.h>
-#include <ArduinoFake.h>
-
+#include <fakeit.hpp>
+#include <Memoria.h>
+#include <Preferences.h>
 
 using namespace fakeit;
 
-struct Preference {
-    virtual int Begin(void);
-    virtual void End(void);
-    virtual void putString(void);
-    virtual void getString(void);
-};
-
-Mock<Preference> Preference_mock;
-
-int Begin(void){
-   return Preference_mock.get().Begin();
-}
-void End(void){
-    Preference_mock.get().End();
-}
-void putString (void){
-    Preference_mock.get().putString();
-}
-void getString (void){
-    Preference_mock.get().getString();
-}
+Mock<Preferences> preferencesMock;
 
 void setUp(void){}
 
 void tearDown(void){}
 
-
 void test_GuardarDatos(void){
-    When(Method(Preference_mock, Begin));
-    When(Method(Preference_mock, putString));
-    When(Method(Preference_mock, putString));
-    When(Method(Preference_mock, End));
-    Verify(Method(Preference_mock, Begin));
 
-    Verify(Method(Preference_mock, putString));
-    Verify(Method(Preference_mock, putString));
-    Verify(Method(Preference_mock, End));
+    When(Method(preferencesMock, begin)).AlwaysReturn();
+    When(Method(preferencesMock, end)).AlwaysReturn();
+    When(Method(preferencesMock, putString)).AlwaysReturn();
+
+    set_preferences(&preferencesMock.get());
+    GuardarDatos("address", "codigo");
+
+    Verify(Method(preferencesMock, putString)).Exactly(2);
 }
 
-void test_ObtenerMac(void){
-    When(Method(Preference_mock, Begin));
-    When(Method(Preference_mock, getString));
-    When(Method(Preference_mock, End));
-    Verify(Method(Preference_mock, Begin));
-    Verify(Method(Preference_mock, getString));
-    Verify(Method(Preference_mock, End));
-}
+// void test_ObtenerMac(void){
+//     When(Method(Preference_mock, Begin));
+//     When(Method(Preference_mock, getString));
+//     When(Method(Preference_mock, End));
+//     Verify(Method(Preference_mock, Begin));
+//     Verify(Method(Preference_mock, getString));
+//     Verify(Method(Preference_mock, End));
+// }
 
-void test_ObtenerCodigo(void){
-    When(Method(Preference_mock, Begin));
-    When(Method(Preference_mock, getString));
-    When(Method(Preference_mock, End));
-    Verify(Method(Preference_mock, Begin));
-    Verify(Method(Preference_mock, getString));
-    Verify(Method(Preference_mock, End));
+// void test_ObtenerCodigo(void){
+//     When(Method(Preference_mock, Begin));
+//     When(Method(Preference_mock, getString));
+//     When(Method(Preference_mock, End));
+//     Verify(Method(Preference_mock, Begin));
+//     Verify(Method(Preference_mock, getString));
+//     Verify(Method(Preference_mock, End));
     
-}
+// }
 
 int main(int argc, char **argv){
     UNITY_BEGIN();
 
     RUN_TEST(test_GuardarDatos);
-    RUN_TEST(test_ObtenerMac);
-    RUN_TEST(test_ObtenerCodigo);
+    // RUN_TEST(test_ObtenerMac);
+    // RUN_TEST(test_ObtenerCodigo);
 
     UNITY_END();
 
