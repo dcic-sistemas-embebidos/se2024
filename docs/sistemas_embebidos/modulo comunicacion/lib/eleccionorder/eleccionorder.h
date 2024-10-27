71cc82689e382e4aabb9b86549a11dfc7a750ef6
TheoCarrier36 <147029437+TheoCarrier36@users.noreply.github.com>
2024-10-27 17:05:07 -0300

    Update eleccionorder.h

--- docs/sistemas_embebidos/modulo comunicacion/lib/eleccionorder/eleccionorder.h
+++ docs/sistemas_embebidos/modulo comunicacion/lib/eleccionorder/eleccionorder.h
@@ -6,6 +6,6 @@ extern const int LED_PIN; // Declare LED_PIN as an external variable
 void order(char estado);
 void blink(int times, int delayTime);
 void bluetoothsetup(void);
-char orderloop(void);
+void orderloop(void);
 
 #endif
