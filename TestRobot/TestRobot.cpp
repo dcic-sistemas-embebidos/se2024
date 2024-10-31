97f245b6c8515f4f753510baff7749257b2e969a
miguelbustamante2020 <miguelcurso2016@gmail.com>
2024-10-31 19:19:13 -0300

    Test robot

---
+++ TestRobot/TestRobot.cpp
@@ -0,0 +1,42 @@
+#include <Arduino.h>
+#include <LedsController.h>
+#include <move_controller.h>
+
+
+void testrobot_setup(void){
+   Serial.begin(115200);
+   Serial.println("setup robot "); 
+   ledscontroller_setup();  
+   move_controller_setup();
+   
+}
+
+void testrobot_loop(void){   
+   ledscontroller_loop();
+}
+
+void probarleds(void){
+   Serial.println("Comenzo "); 
+   prenderled("rojo");
+   delay(2000);
+   prenderled("amarillo");
+   delay(2000);
+   prenderled("verde");
+   delay(2000);
+   apagarled("amarillo");
+   delay(2000);
+   apagarled("rojo");
+   delay(2000);
+   apagarled("verde");
+}
+
+void probarmotores(void){
+   robot_avanzar();
+   delay(1000);
+   robot_detenerse();
+   delay(1000);
+   robot_girar_izquierda();
+   delay(1000);
+   robot_girar_derecha(); 
+   delay(1000);
+}
---
+++ TestRobot/TestRobot.h
@@ -0,0 +1,8 @@
+#ifndef TestRobot_H
+#define TestRobot_H
+
+void testrobot_setup(void);
+void testrobot_loop(void);
+void probarleds(void);
+void probarmotores(void);
+#endif
