b0585841a18ed5ca846073a6cb3c6067c7361db1
MiguelBustamante <111795113+miguelbustamante2020@users.noreply.github.com>
2024-10-31 19:32:22 -0300

    Update TestRobot.h

--- Test_Robot/lib/TestRobot/TestRobot.h
+++ Test_Robot/lib/TestRobot/TestRobot.h
@@ -1,8 +1,8 @@
 #ifndef TestRobot_H
 #define TestRobot_H
 
-void testrobot_setup(void){}
-void testrobot_loop(void){}
-void probarleds(void){}
-void probarmotores(void){}
+void testrobot_setup(void);
+void testrobot_loop(void);
+void probarleds(void);
+void probarmotores(void);
 #endif
