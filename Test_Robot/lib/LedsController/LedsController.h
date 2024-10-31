5d99f193bb1fd1ab83f60c620b5030c7d3a9e2cc
miguelbustamante2020 <miguelcurso2016@gmail.com>
2024-10-31 18:52:38 -0300

    Test robot

---
+++ Test_Robot/lib/LedsController/LedsController.h
@@ -0,0 +1,9 @@
+#ifndef LedsController_H
+#define LedsController_H
+
+void ledscontroller_setup(void);
+void ledscontroller_loop(void);
+void prenderled(String colorled);
+void apagarled(String colorled);
+
+#endif
---
+++ Test_Robot/src/main.cpp
@@ -0,0 +1,15 @@
+#include <TestRobot.h>
+#include <Arduino.h>
+
+void setup() {
+  testrobot_setup();
+  probarleds();
+  delay(10000);
+  
+}
+
+void loop() {
+ 
+   
+}
+
