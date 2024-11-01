e7788b7154a5484157bb7c21577a7cc138cad61c
pintosmicaela <micaelalujanpintos@gmail.com>
2024-11-01 11:29:43 -0300

    borrando archivos

--- lib/Reset/src/reset_interface.h
+++
@@ -1,8 +0,0 @@
-#ifndef RESET_INTERFACE_H
-#define RESET_INTERFACE_H
-
-void setup_reset(void);
-void loop_reset(void);
-void force_reset(void);
-
-#endif
--- modulo-reset/lib/tarea/tarea.h
+++
@@ -1,6 +0,0 @@
-#ifndef TAREA_H
-#define TAREA_H
-
-void tareaEjemplo (void *parametros);
-
-#endif
