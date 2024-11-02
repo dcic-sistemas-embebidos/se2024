29463f93ed45d2f752b7151e7d2b5e90cef9ae7f
MiguelBustamante <111795113+miguelbustamante2020@users.noreply.github.com>
2024-11-02 18:39:34 -0300

    Delete Test_Robot/lib/LedsController/LedsController.h

--- Test_Robot/lib/LedsController/LedsController.h
+++
@@ -1,9 +0,0 @@
-#ifndef LedsController_H
-#define LedsController_H
-
-void ledscontroller_setup(void);
-void ledscontroller_loop(void);
-void prenderled(String colorled);
-void apagarled(String colorled);
-
-#endif
