e60a0e20ca65e4aef61e294c95810acba73dda90
pintosmicaela <micaelalujanpintos@gmail.com>
2024-11-04 12:24:35 -0300

    106 agregado de funciones

--- lib/lcd_driver/lcd_interface.h
+++ lib/lcd_driver/lcd_interface.h
@@ -2,6 +2,8 @@
 #define LCD_INTERFACE_H
 
     void setup_lcd(void);
-    void loop_lcd(char* msg);
+    void loop_lcd(char* message, int cols, int row);
+    void clean_row_lcd(int row);
+    void clean_lcd(void);
 
 #endif
